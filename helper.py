1053af9e73f63e2ce210a3b028efcbf806a8b304
Jaatulendi <150812541+Jaatulendi@users.noreply.github.com>
2024-01-10 22:20:32 +0530

    Update helper.py

--- helper.py
+++ helper.py
@@ -127,14 +127,14 @@ async def send_vid(bot: Client, m: Message, cc, filename, thumb, name):
     start_time = time.time()
 
     try:
-        copy = await bot.send_video(chat_id=m.chat.id,video=filename,caption=cc, supports_streaming=True,height=720,width=1280,thumb=thumbnail,duration=dur) #, progress=progress_bar,progress_args=(reply,start_time))
+        copy = await bot.send_video(chat_id=m.chat.id,video=filename,caption=cc, supports_streaming=True,height=720,width=1280,thumb=thumbnail,duration=dur) , progress=progress_bar,progress_args=(reply,start_time))
         await copy.copy(chat_id = LOG) 
     except TimeoutError:
         await asyncio.sleep(5) 
-        copy = await bot.send_video(chat_id=m.chat.id,video=filename,caption=cc, supports_streaming=True,height=720,width=1280,thumb=thumbnail,duration=dur) #, progress=progress_bar,progress_args=(reply,start_time))
+        copy = await bot.send_video(chat_id=m.chat.id,video=filename,caption=cc, supports_streaming=True,height=720,width=1280,thumb=thumbnail,duration=dur) , progress=progress_bar,progress_args=(reply,start_time))
         await copy.copy(chat_id = LOG)       
     except Exception:
-        copy = await bot.send_video(chat_id=m.chat.id,video=filename,caption=cc, supports_streaming=True,height=720,width=1280,thumb=thumbnail,duration=dur) #progress=progress_bar,progress_args=(reply,start_time))
+        copy = await bot.send_video(chat_id=m.chat.id,video=filename,caption=cc, supports_streaming=True,height=720,width=1280,thumb=thumbnail,duration=dur) progress=progress_bar,progress_args=(reply,start_time))
         await copy.copy(chat_id = LOG)
 
 
